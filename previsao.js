0b77293e50bf33aa4b27cc80c588c2ba47673f16
Vagner <vagnerlrg@gmail.com>
2024-04-24 14:54:05 -0300

    correção do controle de envio do formulário

--- previsao.js
+++ previsao.js
@@ -6,7 +6,7 @@ async function getPrevisao() {
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m`);
         const data = await response.json();
         console.log(data);
-        document.getElementById('previsao').value += "Previsão da temperatura atual na região: " + data.current.temperature_2m + "ºC";
+        document.getElementById('previsao').innerHTML = "Previsão da temperatura atual na região: " + data.current.temperature_2m + "ºC";
         }
      catch(error) {
         alert(error.message);
